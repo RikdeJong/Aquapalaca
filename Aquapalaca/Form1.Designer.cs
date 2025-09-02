bea59332e40005b12dbbfbccd2302ca465062cc7
lucad <lucadehaas1@gmail.com>
2025-09-02 11:29:52 +0200

    efsfes  hh

--- Aquapalaca/Form1.Designer.cs
+++ Aquapalaca/Form1.Designer.cs
@@ -28,22 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.txtWachtwoord = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtGebruikersnaam
             // 
-            this.textBox1.Location = new System.Drawing.Point(350, 180);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(306, 211);
+            this.txtGebruikersnaam.Name = "txtGebruikersnaam";
+            this.txtGebruikersnaam.Size = new System.Drawing.Size(100, 22);
+            this.txtGebruikersnaam.TabIndex = 0;
+            // 
+            // txtWachtwoord
+            // 
+            this.txtWachtwoord.Location = new System.Drawing.Point(306, 257);
+            this.txtWachtwoord.Name = "txtWachtwoord";
+            this.txtWachtwoord.Size = new System.Drawing.Size(100, 22);
+            this.txtWachtwoord.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtWachtwoord);
+            this.Controls.Add(this.txtGebruikersnaam);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +62,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtGebruikersnaam;
+        private System.Windows.Forms.TextBox txtWachtwoord;
     }
 }
 

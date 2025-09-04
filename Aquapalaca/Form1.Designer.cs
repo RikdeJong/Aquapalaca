69929867d711b26576451ac35553f71004f280ee
lucad <lucadehaas1@gmail.com>
2025-09-04 13:58:47 +0200

    a

--- Aquapalaca/Form1.Designer.cs
+++ Aquapalaca/Form1.Designer.cs
@@ -30,27 +30,38 @@
         {
             this.txtGebruikersnaam = new System.Windows.Forms.TextBox();
             this.txtWachtwoord = new System.Windows.Forms.TextBox();
+            this.btnInloggen = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGebruikersnaam
             // 
-            this.txtGebruikersnaam.Location = new System.Drawing.Point(306, 211);
+            this.txtGebruikersnaam.Location = new System.Drawing.Point(322, 178);
             this.txtGebruikersnaam.Name = "txtGebruikersnaam";
             this.txtGebruikersnaam.Size = new System.Drawing.Size(100, 22);
             this.txtGebruikersnaam.TabIndex = 0;
             // 
             // txtWachtwoord
             // 
-            this.txtWachtwoord.Location = new System.Drawing.Point(306, 257);
+            this.txtWachtwoord.Location = new System.Drawing.Point(322, 224);
             this.txtWachtwoord.Name = "txtWachtwoord";
             this.txtWachtwoord.Size = new System.Drawing.Size(100, 22);
             this.txtWachtwoord.TabIndex = 1;
+            // 
+            // btnInloggen
+            // 
+            this.btnInloggen.Location = new System.Drawing.Point(278, 294);
+            this.btnInloggen.Name = "btnInloggen";
+            this.btnInloggen.Size = new System.Drawing.Size(178, 58);
+            this.btnInloggen.TabIndex = 2;
+            this.btnInloggen.Text = "Inloggen";
+            this.btnInloggen.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnInloggen);
             this.Controls.Add(this.txtWachtwoord);
             this.Controls.Add(this.txtGebruikersnaam);
             this.Name = "Form1";
@@ -64,6 +75,7 @@
 
         private System.Windows.Forms.TextBox txtGebruikersnaam;
         private System.Windows.Forms.TextBox txtWachtwoord;
+        private System.Windows.Forms.Button btnInloggen;
     }
 }
 
---
+++ Aquapalaca/Gebruiker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquapalaca
+{
+    class Gebruiker
+    {
+        public int id;
+        public string gebruikersnaam;
+        public string wachtwoord;
+        public string voorNaam;
+        public string achterNaam;
+        public string email;
+
+
+    }
+}
